9ff475365ab305d0cd2e93c9291f425a450da5ff
AsynchronousMatrix <38085006+4x8Matrix@users.noreply.github.com>
2022-06-05 16:23:40 +0100

    Update Vector2.lua

--- sources/Vector2.lua
+++ sources/Vector2.lua
@@ -13,7 +13,7 @@ function Vector2:__mod(Value)
 
 	if typeValue == "number" then
 		return Vector2.new(self.X % Value, self.Y % Value)
-	elseif typeValue == "table" and typeValue.__type == "Vector2" then
+	elseif typeValue == "table" and Value.__type == "Vector2" then
 		return Vector2.new(self.X % Value.X, self.Y % Value.Y)
 	end
 end
@@ -23,7 +23,7 @@ function Vector2:__div(Value)
 
 	if typeValue == "number" then
 		return Vector2.new(self.X / Value, self.Y / Value)
-	elseif typeValue == "table" and typeValue.__type == "Vector2" then
+	elseif typeValue == "table" and Value.__type == "Vector2" then
 		return Vector2.new(self.X / Value.X, self.Y / Value.Y)
 	end
 end
@@ -33,7 +33,7 @@ function Vector2:__mul(Value)
 
 	if typeValue == "number" then
 		return Vector2.new(self.X * Value, self.Y * Value)
-	elseif typeValue == "table" and typeValue.__type == "Vector2" then
+	elseif typeValue == "table" and Value.__type == "Vector2" then
 		return Vector2.new(self.X * Value.X, self.Y * Value.Y)
 	end
 end
@@ -41,7 +41,7 @@ end
 function Vector2:__sub(Value)
 	local typeValue = type(Value)
 
-	if typeValue == "table" and typeValue.__type == "Vector2" then
+	if typeValue == "table" and Value.__type == "Vector2" then
 		return Vector2.new(self.X - Value.X, self.Y - Value.Y)
 	end
 end
@@ -49,7 +49,7 @@ end
 function Vector2:__add(Value)
 	local typeValue = type(Value)
 
-	if typeValue == "table" and typeValue.__type == "Vector2" then
+	if typeValue == "table" and Value.__type == "Vector2" then
 		return Vector2.new(self.X + Value.X, self.Y + Value.Y)
 	end
 end
@@ -59,13 +59,25 @@ function Vector2:__unm()
 end
 
 -- // Functions
+function Vector2:Max(Vector)
+	return Vector2.new(
+		(Vector.X > self.X and Vector.X) or self.X,
+		(Vector.Y > self.Y and Vector.Y) or self.Y
+	)
+end
+
+function Vector2:Min(Vector)
+	return Vector2.new(
+		(Vector.X < self.X and Vector.X) or self.X,
+		(Vector.Y < self.Y and Vector.Y) or self.Y
+	)
+end
+
 function Vector2:Cross(Vector)
-	return 
+	return (Vector.Y * self.X) - (Vector.X * self.Y)
 end
 
 function Vector2:Angle(Vector)
-	print(self:Dot(Vector), Vector.Magnitude, self.Magnitude)
-
 	return math.deg(math.acos(self:Dot(Vector) / (Vector.Magnitude * self.Magnitude)))
 end
 
@@ -74,7 +86,11 @@ function Vector2:Dot(Vector)
 end
 
 function Vector2:Lerp(Vector, Alpha)
-	return 1
+	return self + (Vector - self) * Alpha
+end
+
+function Vector2:Normalise()
+	return self / self.Magnitude
 end
 
 function Vector2:Unpack()
@@ -85,7 +101,6 @@ function Vector2.new(X, Y)
 	local self = setmetatable({ X = X or 0, Y = Y or 0 }, Vector2)
 
 	self.Magnitude = math.sqrt((self.X ^ 2) + (self.Y ^ 2))
-	self.Unit = self / self.Magnitude
 
 	return self
 end
